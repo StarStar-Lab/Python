fd09f49f85cb40239118a1438da69de31303d300
StarStar-Lab <35412541+StarStar-Lab@users.noreply.github.com>
2021-05-17 10:34:21 +0800

    Create bing.py

---
+++ bing.py
@@ -0,0 +1,43 @@
+import random
+from time import sleep
+from requests import get
+from pagermaid.listener import listener
+from os import remove
+
+
+@listener(is_plugin=True, outgoing=True, command="bing",
+          description="随机获取壁纸")
+async def ghs(context):
+    await context.edit("搜索壁纸中 . . .")
+    status = False
+    index = random.randint()
+    url =  f"https://bing.biturl.top/?resolution=1920&format=image&index=`{str(index)}`&mkt=zh-CN"
+    for _ in range (20): #最多重试20次
+        website = random.randint(0,0)
+        filename = "wallpaper" + str(random.random())[2:] + ".png"
+        try:
+            if website == 0:
+                img = get(url)
+            if img.status_code == 200:
+                with open(filename, 'wb') as f:
+                    f.write(img.content)
+                await context.edit("传壁纸中 . . .")
+                await context.client.send_file(context.chat_id,filename,caption="#Wallpaper")
+                status = True
+                break #成功了就赶紧结束啦！
+        except:
+            try:
+                remove(filename)
+            except:
+                pass
+            continue
+    try:
+        remove(filename)
+    except:
+        pass
+    try:
+        await context.delete()
+    except:
+        pass
+    if not status:
+        await context.client.send_message(context.chat_id,"出错了呜呜呜 ~ 试了好多好多次都无法访问到服务器 。")
